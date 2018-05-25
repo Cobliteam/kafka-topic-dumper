9ef865a98b3088b58fbf245bad62dd691d3228e6
Evandro Sanches <evandro@cobli.co>
2018-05-25 17:09:37 -0300

    main: Add command line options

--- kafka-topic-dumper/kafka_topic_dumper/main.py
+++ kafka-topic-dumper/kafka_topic_dumper/main.py
@@ -1,5 +1,7 @@
 import logging
 
+import argparse
+
 from kafka_topic_dumper.kafka_client import KafkaClient
 
 
@@ -7,18 +9,53 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+
 def main():
     logging.basicConfig(
-        format='%(name)s - %(levelname)s - %(message)s')
+        format='%(asctime)s: %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
 
-    topic = 'test'
-    bootstrap_servers = 'localhost:9092'
+    parser = argparse.ArgumentParser(
+        description='Simple tool to dump kafka messages and send it to AWS S3')
+
+    parser.add_argument('-t', '--topic', default='kafka-topic-dumper',
+                        help='Kafka topic to fetch messages from.')
+
+    parser.add_argument('-s', '--bootstrap-servers', default='localhost:9092',
+                        help='host[:port] string (or list of host[:port] '
+                             'strings) that the consumer should contact to '
+                             'bootstrap initial cluster metadata. If no '
+                             'servers are specified, will default to '
+                             'localhost:9092.')
+
+    parser.add_argument('-n', '--num-messages', default=300,
+                        help='Number of messages to try dump.')
+
+    parser.add_argument('-p', '--path', default='./data',
+                        help='Path to folder where storage local dump files.')
+
+    parser.add_argument('-m', '--max-messages-per-package', default=100,
+                        help='Maximum number of messages per dump file.')
+
+    parser.add_argument('-d', '--dry-run', action='store_true',
+                        help='In dry run mode, kafka-topic-dumper will '
+                             'generate local files. But will not send it to '
+                             'AWS S3 bucket.')
+
+    parser.add_argument('-b', '--bucket-name', default='kafka-topic-dumper',
+                        help='Maximum number of messages per dump file.')
+
+
+    opts = parser.parse_args()
+
+    topic = opts.topic
+    bootstrap_servers = opts.bootstrap_servers
+    num_messages_to_consume = opts.num_messages
+    max_messages_per_package = opts.max_messages_per_package
+    dir_path_to_save_files = opts.path
+    dry_run = opts.dry_run
     group_id = 'kafka_topic_dumper'
-    num_messages_to_consume = 300  # 000
-    max_package_size_in_msgs = 50  # 000
-    dir_path_to_save_files = 'data'
-    bucket_name = 'cobli-alexstrasza-stress-test'
-    dry_run = False
+    bucket_name = opts.bucket_name
 
     kafka_client = KafkaClient(
         topic=topic,
@@ -27,7 +64,7 @@ def main():
 
     kafka_client.get_messages(
         num_messages_to_consume=num_messages_to_consume,
-        max_package_size_in_msgs=max_package_size_in_msgs,
+        max_package_size_in_msgs=max_messages_per_package,
         dir_path=dir_path_to_save_files,
         bucket_name=bucket_name,
         dry_run=dry_run)
