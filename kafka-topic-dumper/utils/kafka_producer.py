7b5a39a2f3bb85044cbf2a772d7376d5df924376
Evandro Sanches <evandro@cobli.co>
2018-05-28 15:36:37 -0300

    kafka_producer: Fix logger and rearrange script flow

--- kafka-topic-dumper/utils/kafka_producer.py
+++ kafka-topic-dumper/utils/kafka_producer.py
@@ -2,20 +2,28 @@ import logging
 
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
 def on_send_success(record_metadata):
     print(record_metadata.topic)
     print(record_metadata.partition)
     print(record_metadata.offset)
 
+
 def on_send_error(excp):
-    log.error('I am an errback', exc_info=excp)
+    logger.error('I am an errback', exc_info=excp)
     # handle exception
+
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+logging.basicConfig()
+logger = logging.getLog(__name__)
+logger.setLevel(logging.INFO)
 
 # produce asynchronously
 msg = ('Message to send to kafka as a value. ' +
        'This message is the test message {:015d}')
+
 for n in range(300000):
     producer.send(
         'test',
